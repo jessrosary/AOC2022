b214bd65acf7fc7945ea4a3b47bfb33510dfd6b8
Jess Rose <jrosehow@gmail.com>
2022-12-04 20:37:30 -0500

    revise map functions to be nicer to look at

--- src/dayFour.js
+++ src/dayFour.js
@@ -4,17 +4,13 @@ const data = readFileSync('./data/input04.txt', 'utf8')
   .trim()
   .split('\n')
   .map((line) =>
-    line.split(',').map((arr) => arr.split('-').map((int) => parseInt(int, 10)))
+    line.split(',').map((arr) => arr.split('-').map((x) => parseInt(x)))
   );
 
 //Part One
 const checkRangeEncompass = (arr) => {
   let count = 0;
-  arr.map((x) => {
-    const leftMin = x[0][0];
-    const leftMax = x[0][1];
-    const rightMin = x[1][0];
-    const rightMax = x[1][1];
+  arr.map(([[leftMin, leftMax], [rightMin, rightMax]]) => {
     if (leftMin <= rightMin && leftMax >= rightMax) {
       count += 1;
     } else if (rightMin <= leftMin && rightMax >= leftMax) {
@@ -27,11 +23,7 @@ const checkRangeEncompass = (arr) => {
 //Part Two
 const checkRangeOverlap = (arr) => {
   let count = 0;
-  arr.map((x) => {
-    const leftMin = x[0][0];
-    const leftMax = x[0][1];
-    const rightMin = x[1][0];
-    const rightMax = x[1][1];
+  arr.map(([[leftMin, leftMax], [rightMin, rightMax]]) => {
     if (leftMin <= rightMin && rightMin <= leftMax) {
       count += 1;
     } else if (rightMin <= leftMin && leftMin <= rightMax) {
